3e5fd0f6351b8ad350b296a510507757532a09e6
mrhonest111@hotmail.com <crs-94@hotmail.com>
2015-09-29 23:42:49 -0700

    done

---
+++ inet.h
@@ -0,0 +1,26 @@
+/*	inet.h file
+	
+	TTP3121 TCP/IP Programming Project (Trimester 1, 2015/16) 
+	Members: 1) Aloka Heu Rui Min 1101108810*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
+
+
+
+
+
+
+
